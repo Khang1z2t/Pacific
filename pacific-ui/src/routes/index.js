fa9f00f256d738eaceac92308e6e4e78cd1d1677
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2025-01-17 22:02:58 +0700

    add project base fe

---
+++ pacific-ui/src/routes/index.js
@@ -0,0 +1,11 @@
+import Home from '~/pages/Home';
+
+// Public Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+];
+
+// Private Routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
