5ab029e88b3cbad27a2e515e6d113c4f7fc4ea22
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2025-02-15 22:50:16 +0700

    fix main

--- pacific-ui/src/pages/TourLists/TourLists_outside.jsx
+++
@@ -1,7 +0,0 @@
-export const TourLists_outside = () => {
-    return (
-        <>
-        HI
-        </>
-    );
-};
--- pacific-ui/src/pages/TourLists/data/prices.js
+++
@@ -1,5 +0,0 @@
-export const prices = [
-    { value: "All", label: "Tất cả giá" },
-    { value: "HighToLow", label: "Cao nhất -> Thấp nhất" },
-    { value: "LowToHigh", label: "Thấp nhất -> Cao nhất" }
-];
