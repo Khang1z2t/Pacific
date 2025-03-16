c16e4a1fb38c536a987bd3a736ba6c478e1b3ac6
TN1608 <tuanndps36835@fpt.edu.vn>
2025-03-16 19:06:55 +0700

    Fix import

--- pacific-ui/src/pages/Account/ForgetPassword.jsx
+++ pacific-ui/src/pages/Account/ForgetPassword.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import config from '~/config';
 import anhBien from '~/pages/Account/imgAC/anh-bien.jpg';
 import logo from '~/pages/Account/imgAC/logo.jpg';
-import { resetPassword } from '~/config/firebase/auth';
 import AuthServices from '~/services/AuthServices';
 
 export const ForgetPassword = () => {
