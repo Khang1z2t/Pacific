ae31c239556a7767d469eb3a34b74e0adce87300
TN1608 <tuanndps36835@fpt.edu.vn>
2025-04-03 16:54:41 +0700

    Enhance UI

--- pacific-ui/src/index.js
+++ pacific-ui/src/index.js
@@ -10,7 +10,7 @@ import './i18n';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <React.Suspense fallback="loading...">
+        <React.Suspense>
         <GlobalStyles>
             <AuthProvider>
                 <App />
