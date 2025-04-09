6b3e868a7aafabfa58d3e38e6b10f0e7e6967bbe
TN1608 <tuanndps36835@fpt.edu.vn>
2025-04-09 17:25:58 +0700

    ENHANCE UI

--- pacific-ui/src/config/imageConfig.js
+++ pacific-ui/src/config/imageConfig.js
@@ -1,18 +1,9 @@
-import config from '~/config/index';
-
 const imageConfig = {
     getImage: (imageId) => {
-        return `http://localhost:8080/api/image/${imageId}`;
+            return `http://localhost:8080/api/image/${imageId}`;
     },
     getAvatar: (imageId) => {
-        if (!imageId) {
-            return config.webConfig.defaultUser; // Ảnh mặc định nếu không có imageId
-        }
-        // Giả định: Drive ID dài hơn 20 ký tự, Google ID ngắn hơn
-        const isDriveId = imageId.length > 20 || imageId.includes('-'); // Điều chỉnh điều kiện dựa trên thực tế
-        return isDriveId
-            ? `https://lh3.googleusercontent.com/d/${imageId}`
-            : `https://lh3.googleusercontent.com/a/${imageId}`;
+        return `https://lh3.googleusercontent.com/a/${imageId}`;
     },
 };
 
