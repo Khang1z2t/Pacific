fc872cd31c11bc053f7b391ffa380f024df01be4
TN1608 <tuanndps36835@fpt.edu.vn>
2025-04-27 18:47:21 +0700

    fix imgconfig

--- pacific-ui/src/config/imageConfig.js
+++ pacific-ui/src/config/imageConfig.js
@@ -1,6 +1,12 @@
+import config from '~/config/index';
+import axiosConfig from '~/config/axiosConfig';
+
 const imageConfig = {
+    // getImage: (imageId) => {
+    //         return `http://localhost:8080/api/image/${imageId}`;
+    // },
     getImage: (imageId) => {
-            return `http://localhost:8080/api/image/${imageId}`;
+        return `https://khangyuno.id.vn/api/image/${imageId}`;
     },
     getAvatar: (imageId) => {
         return `https://lh3.googleusercontent.com/a/${imageId}`;
